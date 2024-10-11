6dafbaef26163c79ca1e224f4be7429a7a980b34
lewpar <lewis.o.parmenter@gmail.com>
2024-10-11 19:37:52 +1100

    Replaced hugo website with ASP.NET website + CLI CMS projects.

---
+++ CyberBilby.CMS/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CyberBilby.CMS;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ CyberBilby.Shared/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace CyberBilby.Shared;
+
+public class Class1
+{
+}
